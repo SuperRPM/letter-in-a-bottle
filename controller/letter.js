9d1e11c82dea27acd66e149fb2bb33fd7c57bf34
SuperRPM <vidafirmar@gmail.com>
2021-08-24 14:17:14 +0900

    add getLetter func in letter cntr

--- controller/letter.js
+++ controller/letter.js
@@ -3,8 +3,20 @@ import * as userData from '../data/letter'
 
 export function getAllLetter(req, res) {
     const account = req.query.account;
-    const data = userData.getAllLettrByAccount(account);
+    const data = await userData.getAllLettrByAccount(account);
     if (data) {
         res.status(200).json(data);
+    } else {
+        res.status(400).json({ message: 'letter controller getAllLetter error'})
+    }
+}
+
+export function getLetter(req, res) {
+    const id = req.param.id;
+    const letter = await userData.gettAllLetterbyId(id);
+    if (letter) {
+        res.status(200).json(letter);
+    } else {
+        res.status(404).json({ message: 'letter controller GetLetter error'});
     }
 }
