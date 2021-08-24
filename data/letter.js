b2157218bf706fafb4c1642b8c245cd5c549d26c
SuperRPM <vidafirmar@gmail.com>
2021-08-24 20:21:30 +0900

    add funcs in data letter

---
+++ data/letter.js
@@ -0,0 +1,16 @@
+// getAllLetterByAccount getAllLetterById createLetter deleteLetter
+export async function gettAllLetterByAccount(account) {
+    return null
+}
+
+export async function getAllLetterById(id) {
+    return null
+}
+
+export async function createLetter(userId, text) {
+    return null
+}
+
+export async function deleteLetter(id) {
+    return null
+}
