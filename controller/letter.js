84bdc186015328885cde0116ddaf45bc9e5f075a
SuperRPM <vidafirmar@gmail.com>
2021-08-24 14:36:16 +0900

    add removeLetter func letter cntr

--- controller/letter.js
+++ controller/letter.js
@@ -1,9 +1,9 @@
-import * as userData from '../data/letter'
+import * as letterData from '../data/letter'
 // getAllLetter getLetter createLetter removeLetter
 
 export function getAllLetter(req, res) {
     const account = req.query.account;
-    const data = await userData.getAllLettrByAccount(account);
+    const data = await letterData.getAllLettrByAccount(account);
     if (data) {
         res.status(200).json(data);
     } else {
@@ -13,10 +13,29 @@ export function getAllLetter(req, res) {
 
 export function getLetter(req, res) {
     const id = req.param.id;
-    const letter = await userData.gettAllLetterbyId(id);
+    const letter = await letterData.gettAllLetterbyId(id);
     if (letter) {
         res.status(200).json(letter);
     } else {
         res.status(404).json({ message: 'letter controller GetLetter error'});
     }
+}
+
+export function postLetter(req, res) {
+    const {text} = req.body;
+    const letter = await letterData.createLtter(text, req.userId);
+    res.status(201).json(letter);
+}
+
+export function removeLetter(req, res) {
+    const id = req.param.id;
+    const isValid = await letterData.gettAllLetterbyId(id);
+    if (!isValid) {
+        return res.sendStatus(404);
+    }
+    if (isValid.userId !== req.userId) {
+        return res.sendStatus(403);
+    }
+    await letterData.deleteLetter(id);
+    res.sendStatus(204);
 }
