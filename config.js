d9231d3c18e3d38abacb3ee565fc2f356400356e
SuperRPM <vidafirmar@gmail.com>
2021-08-25 22:30:51 +0900

    update config.js jwt, bcrypt, host

--- config.js
+++ config.js
@@ -9,6 +9,16 @@ function required(key, defaultValue = undefined) {
     return value
 }
 export const config = {
+    jwt: {
+        secretKey: required('JWT_SECRET'),
+        expiresInSec: parseInt(required('JWT_EXPIRES_SEC', 43200)),
+    },
+    bcrypt: {
+        saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS', 12))
+    },
+    host: {
+        port: parseInt(required('HOST_PORT', 8080))
+    },
     db: {
         host: required('DB_HOST'),
         user: required('DB_USER'),
