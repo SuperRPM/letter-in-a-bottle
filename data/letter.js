f96ba6dd66abe63130a5b7ab98076742de5d187d
SuperRPM <vidafirmar@gmail.com>
2021-08-25 17:13:16 +0900

    update getAllLetterByAccount in letter data

--- data/letter.js
+++ data/letter.js
@@ -1,7 +1,30 @@
-// getAllLetterByAccount getAllLetterById createLetter deleteLetter
+import { Sequelize } from "sequelize/types";
+import { Letter, User } from "../db/database.js"
+
+const INCLUDE_USER = {
+    attribues: [
+        'id',
+        'text',
+        'createdAt',
+        'userId',
+        [Sequelize.col('user.name'), 'name'],
+        [Sequelize.col('user.account'), 'account'],
+        [Sequelize.col('user.url'), 'url'],
+    ]
+};
+
+const ORDER_DESC = { order: [['createdAt', 'DESC']] };
+
 export async function gettAllLetterByAccount(account) {
-    return null
-}
+    return Letter.findAll({
+        ...INCLUDE_USER,
+        ...ORDER_DESC,
+        include: {
+            ...INCLUDE_USER.include,
+            where: {account},
+        },
+    });
+};
 
 export async function getAllLetterById(id) {
     return null
