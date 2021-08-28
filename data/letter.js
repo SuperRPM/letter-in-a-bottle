ccb74df8330adacaab196be3fbc5e6eeb515885a
SuperRPM <vidafirmar@gmail.com>
2021-08-29 04:41:58 +0900

    add getUnrepliedLetter(need more update)

--- data/letter.js
+++ data/letter.js
@@ -1,4 +1,5 @@
-import { Letter, User } from "../db/database.js"
+import { Letter, User } from "../db/database.js";
+import { findByAccount } from '../data/auth.js';
 import pkg from 'sequelize';
 const { Sequelize } = pkg;
 
@@ -8,7 +9,7 @@ const JOIN_USER = {
         'text',
         'createdAt',
         'userId',
-        'replied',
+        'receiver',
         [Sequelize.col('user.name'), 'name'],
         [Sequelize.col('user.account'), 'account'],
         [Sequelize.col('user.url'), 'url'],
@@ -44,12 +45,30 @@ export async function deleteLetter(id) {
     return Letter.findByPk(id).then((letter) => letter.destroy());
 }
 
-export async function getUnrepliedLetter() {
-    return Letter.findAll({
+export async function getUnrepliedLetter(userId) {
+    const unrepliedLetter = await Letter.findAll({
         ...JOIN_USER,
-        where: {replied: false},
+        where: { receiver: 0 },
+        // 자기편지를 받으면 안되고 이미 편지를 가진 유저가 또 받으면 안되는데 어떻게 해야하나
         include: {
             ...JOIN_USER.include,
         },
     });
+    const length = unrepliedLetter.length;
+    if (length === 0) {
+        return false;
+    }
+    const random = Math.round(Math.random() * 10);
+    const index = random % length;
+    const letterId = unrepliedLetter[index].dataValues.id;
+    User.findByPk(userId)
+    .then((user) => {
+        user.mail = true;
+        user.save();
+    })
+    return Letter.findByPk(letterId, JOIN_USER)
+    .then((letter) => {
+        letter.receiver = userId;
+        return letter.save();
+    })
 }
