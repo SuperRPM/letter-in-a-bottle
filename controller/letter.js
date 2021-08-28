6afeaabb9429ddccc5fb339a7828d3bc753093f7
SuperRPM <vidafirmar@gmail.com>
2021-08-29 00:55:55 +0900

    add getRandomLetter func in letter cntr

--- controller/letter.js
+++ controller/letter.js
@@ -21,6 +21,16 @@ export async function getLetter(req, res) {
     }
 }
 
+// createdAt으로 만들어진지 며칠만에 도착한 편지인지도 알려주자. 근데 그건 어디서 하지???
+export async function getRandomLetter(req, res) {
+    const allUnrepliedLetter = await letterData.getUnrepliedLetter();
+    const length = allUnrepliedLetter.length;
+    const random = Math.round(Math.random() * 10);
+    const index = random % length;
+    // console.log((Date.now() - allUnrepliedLetter[index].dataValues.createdAt)/60/60/24/1000);
+    res.status(200).json(allUnrepliedLetter[index]);
+}
+
 export async function postLetter(req, res) {
     const {text} = req.body;
     const letter = await letterData.createLetter(text, req.userId);
