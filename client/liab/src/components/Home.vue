94e188e40ca852740fa6c7cc64b1b4ed6d548938
SuperRPM <vidafirmar@gmail.com>
2021-10-05 00:09:36 +0900

    modify intro text

--- client/liab/src/components/Home.vue
+++ client/liab/src/components/Home.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="container mt-4">
+    <div class="container mt-4 intro">
         <h5>Letter in bottle</h5>
-        <p>누구에게도 하지 못할 하고싶은 말을</p>
+        <p>누구에게도 하지 못할 말을</p>
         <p>아무에게라도 하고싶을 때가 있습니다.</p>
         <p>어린 소년이 하고싶은 말을담아 바다로 보낸편지가</p>
-        <p>노인되어 수천 킬로미터 떨어진곳에서 답장을 받는 마법같은 일이 생기곤 합니다.</p>
-        <p>letter in bottle은 이런 마법같은 일을 바라며 만들었습니다.</p>
+        <p>노인되어 수 천 킬로미터 떨어진 곳에서 답장을 받는 일이 생기곤 합니다.</p>
+        <p>LETTER IN BOTTLE은 이런 마법같은 일을 바라며 만들었습니다.</p>
         <p>답장을 받을지는, 그리고 그 답장에 마음에 들지는 모릅니다.</p>
-        <p><del>사용자가 없으니까요</del></p>
+        <p id="dog-dirp"><del>사용자가 없으니까요</del></p>
         <p>마법 같은 일이 일어나지 않아도 괜찮을겁니다.</p>
         <p>왜냐면 우린 드디어 꼭 하고싶었던 말을 해냈으니까요.</p>
     </div>
@@ -21,5 +21,9 @@ export default {
 </script>
 
 <style>
-
+.intro {
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    color: black;
+    background-color: rgba(255, 255, 255, 0.3);
+}
 </style>
